96ca64ddda009d1d4a3e52fba53418219e55383f
jromero04 <jromero4@correo.um.edu.uy>
2025-05-29 18:07:26 -0300

    Clase coleccion

---
+++ src/main/java/um/edu/uy/entities/Coleccion.java
@@ -0,0 +1,8 @@
+package um.edu.uy.entities;
+
+public class Coleccion {
+    private String id_coleccion;
+    private String titulo_coleccion;
+    // definir guardar las peliculas por la que esta conformada la coleccion
+    // ingresos totales de la coleccion
+}
