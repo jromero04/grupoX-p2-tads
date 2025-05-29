54c3fd9ef1be5ee57987dca3b1e6866fb0c8e1ba
jromero04 <jromero4@correo.um.edu.uy>
2025-05-29 18:04:41 -0300

    Anotaciones en clase Calificacion

--- src/main/java/um/edu/uy/entities/Calificacion.java
+++ src/main/java/um/edu/uy/entities/Calificacion.java
@@ -1,5 +1,8 @@
 package um.edu.uy.entities;
 
 public class Calificacion {
-
+    // id de usuario
+    // pelicula a la que se le hace la calificacion
+    private int puntuacion;
+    // fecha - ver formato en el archivo de ratings
 }
