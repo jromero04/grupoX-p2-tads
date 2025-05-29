1351aa9ddc6ddff04a6f577112d3d20db00ef390
jromero04 <jromero4@correo.um.edu.uy>
2025-05-29 18:09:39 -0300

    Clase Participante

---
+++ src/main/java/um/edu/uy/entities/Participante.java
@@ -0,0 +1,7 @@
+package um.edu.uy.entities;
+
+public class Participante {
+    private String nombre_participante;
+    private String rol;
+    // guardar lista de peliculas en la que participo
+}
