d1f024fd23974bea620a6b8cfc4f7765735d336c
juanignacio0317 <juanignacio0317@gmail.com>
2025-06-09 20:23:13 -0300

    empece consulta 2

--- src/main/java/um/edu/uy/UMovieService.java
+++ src/main/java/um/edu/uy/UMovieService.java
@@ -1,8 +1,10 @@
 package um.edu.uy;
 
 import um.edu.uy.entities.*;
+import um.edu.uy.tads.List.MyLinkedList;
 import um.edu.uy.tads.List.MyList;
 import um.edu.uy.tads.hash.MyHash;
+import um.edu.uy.tads.hash.Node;
 
 public class UMovieService {
     private MyHash<String, Pelicula> peliculas;
@@ -12,5 +14,30 @@ public class UMovieService {
 
     private MyList<Calificacion> calificaciones;
 
-    for
+
+    public void mejorCalificacion(MyList<Calificacion> calificaciones){
+
+    }
+
+    public void mejorCalificacion(MyHash<String, Pelicula> peliculas) {
+        Node<String, Pelicula>[] arreglo = peliculas.getArray();
+        MyList<MyLinkedList> pelicu
+        for (Node<String, Pelicula> nodo : arreglo) {
+            if (nodo != null) {
+                Pelicula p = nodo.getValue();
+                Double totalPuntaje = 0.0;
+                for (int index = 0; index < p.getCalificaciones().size(); index++) {
+                    Double puntaje = p.getCalificaciones().get(index).getPuntaje();
+                    totalPuntaje += puntaje;
+                }
+                double cantidadCalificaciones = p.getCantidadDeCalificaciones();
+                double clasificacionMedia = totalPuntaje/cantidadCalificaciones;
+
+                System.out.println(p.getTitulo_pelicula());
+            }
+
+
+        }
+    }
 }
+
