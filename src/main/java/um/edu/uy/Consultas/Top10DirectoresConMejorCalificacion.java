8d86c4a6b53d067cb7bc75afc13dda165bebb4bc
jromero04 <jromero4@correo.um.edu.uy>
2025-06-27 19:20:08 -0300

    Arreglo en Top 10 directores

--- src/main/java/um/edu/uy/Consultas/Top10DirectoresConMejorCalificacion.java
+++ src/main/java/um/edu/uy/Consultas/Top10DirectoresConMejorCalificacion.java
@@ -23,7 +23,7 @@ public class Top10DirectoresConMejorCalificacion {
         this.servicio = servicio;
     }
 
-    public void ejecutar() throws InvalidHashKey {
+    /*public void ejecutar() throws InvalidHashKey {
         ArrayHeap<Participante> heap = new ArrayHeap<>(false); // false = heap máximo
 
         long inicio = System.currentTimeMillis();
@@ -50,9 +50,9 @@ public class Top10DirectoresConMejorCalificacion {
                     todasLasNotas.add(califs.get(k).getPuntaje());
                 }
             }
-            /*System.out.println("Director: " + director.getNombreParticipante() +
+            System.out.println("Director: " + director.getNombreParticipante() +
                     " | Películas: " + pelisDelDirector.getSize() +
-                    " | Calificaciones: " + todasLasNotas.size());*/
+                    " | Calificaciones: " + todasLasNotas.size());
 
 
             if (todasLasNotas.size() <= 100) continue;
@@ -72,7 +72,49 @@ public class Top10DirectoresConMejorCalificacion {
 
         long fin = System.currentTimeMillis();
         System.out.println("Tiempo de ejecucion de la consulta: " + (fin - inicio) + " ms");
+    }*/
+
+    public void ejecutar() throws InvalidHashKey {
+        ArrayHeap<InfoDirector> heap = new ArrayHeap<>(false); // false = heap máximo
+
+        Node<String, Participante>[] array = servicio.getParticipantes().getArray();
+        for (Node<String, Participante> nodo : array) {
+            if (nodo == null) continue;
+            Participante director = nodo.getValue();
+
+            if (!director.getRol().equals("Director")) continue;
+
+            MyHash<String, Pelicula> pelisDelDirector = director.getPeliculas();
+            if (pelisDelDirector.getSize() <= 1) continue;
+
+            MyList<Double> todasLasNotas = new MyLinkedList<>();
+
+            Node<String, Pelicula>[] nodosPeliculas = pelisDelDirector.getArray();
+            for (Node<String, Pelicula> nodoP : nodosPeliculas) {
+                if (nodoP == null) continue;
+                Pelicula peli = nodoP.getValue();
+                MyList<Calificacion> califs = peli.getCalificaciones();
+
+                for (int k = 0; k < califs.size(); k++) {
+                    todasLasNotas.add(califs.get(k).getPuntaje());
+                }
+            }
+
+            if (todasLasNotas.size() <= 100) continue;
+
+            double mediana = calcularMediana(todasLasNotas);
+            InfoDirector info = new InfoDirector(director.getNombreParticipante(), pelisDelDirector.getSize(), mediana);
+            heap.insert(info);
+        }
+
+        int contador = 0;
+        while (!heap.isEmpty() && contador < 10) {
+            InfoDirector d = heap.delete();
+            System.out.println(d.getNombre() + ", " + d.getCantidadPeliculas() + ", " + String.format("%.2f", d.getMediana()));
+            contador++;
+        }
     }
+
 
     private double calcularMediana(MyList<Double> lista) {
         int n = lista.size();
@@ -87,4 +129,34 @@ public class Top10DirectoresConMejorCalificacion {
             return (copia[n / 2 - 1] + copia[n / 2]) / 2.0;
         }
     }
+
+    private static class InfoDirector implements Comparable<InfoDirector>{
+        private String nombre;
+        private int cantidadPeliculas;
+        private double mediana;
+
+        public InfoDirector(String nombre, int cantidadPeliculas, double mediana) {
+            this.nombre = nombre;
+            this.cantidadPeliculas = cantidadPeliculas;
+            this.mediana = mediana;
+        }
+
+        public String getNombre() {
+            return nombre;
+        }
+
+        public int getCantidadPeliculas() {
+            return cantidadPeliculas;
+        }
+
+        public double getMediana() {
+            return mediana;
+        }
+
+        @Override
+        public int compareTo(InfoDirector otro) {
+            return Double.compare(otro.mediana, this.mediana);
+        }
+    }
+
 }
